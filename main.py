0256d69239b81d825b37fa51b3b5b54295fd0d17
gavgavded-code <gavgavded@gmail.com>
2025-10-02 20:58:25 +0300

    pipiska

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = FastAPI()
  
 @app.get("/")
 def read_root():
-    html_content = "<h2>Hello METANIT.COM!</h2>"
+    html_content = "<h2>pipiska</h2>"
     return HTMLResponse(content=html_content)
